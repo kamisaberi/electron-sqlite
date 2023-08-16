2952f5407b6545141bb6e768d805343251294e23
kamisaberi <kamisaberi@gmail.com>
2023-08-16 09:43:17 +0330

    initial release

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+
+var knex = require("knex")({
+  client: "sqlite3",
+  connection: {
+    filename: path.join(__dirname, "database.sqlite"),
+  },
+});
+app.on("ready", () => {
+  let mainWindow = new BrowserWindow({
+    height: 800,
+    width: 800,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    nodeIntegration: true,
+  });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+  ipcMain.on("mainWindowLoaded", function () {
+    let result = knex.select("FirstName").from("User");
+    result.then(function (rows) {
+      mainWindow.webContents.send("resultSent", rows);
+    });
+  });
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
